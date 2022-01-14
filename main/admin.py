ddcda11823ae704c7ab558fb9c7c8f6d3c5df65c
Jakut <jakut040702@gmail.com>
2022-01-14 20:26:17 +0600

    123

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from main.models import Music, Category, Comment, Favorites
+from main.models import Music, Category, Comment, Favorites, Rating
 
 admin.site.register(Music)
 admin.site.register(Category)
 admin.site.register(Favorites)
 admin.site.register(Comment)
+admin.site.register(Rating)
